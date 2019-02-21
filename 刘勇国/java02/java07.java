5ba5fdf0b1fb896d81ed6532b0b9b2419d39e811
chan-kate <ccsu_loader@163.com>
2019-02-21 17:22:52 +0800

    提交第4章的课件

--- 刘勇国/java02/java07.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class java07{
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		System.out.println("**********************消费单**********************\n购买物品\t单价\t\t个数\t\t金额\nT恤\t\t245\t\t2\t\t490\n网球鞋\t\t570\t\t1\t\t570\n网球拍\t\t320\t\t1\n\n折扣：\t\t8折\n消费总金额\t\t1104.0\n实际消费\t\t1500\n找钱\t\t\t396.0\n本次购物所获的积分是："+(1500/100));
-		
-	}
-}
