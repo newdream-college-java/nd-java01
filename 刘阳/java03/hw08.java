20a43469f6b9a4eb5e6b64293901558f32390b0b
LiuYang87483587 <87483587@qq.com>
2019-02-21 13:07:09 +0800

    第三章作业

---
+++ 刘阳/java03/hw08.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class hw08{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("请输入4位会员号：");
+		int a =input.nextInt();
+		if(a>+1000&&a<+9999){
+			System.out.print(a+"号客户，谢谢您的支持！");
+		}else{
+			System.out.print("输入的会员号错误！");
+		}
+	}
+
+}
