6efc1902c87de8dc095b0804f9bee503101c0686
yangbin1997 <82408624@qq.com>
2019-02-22 08:19:34 +0800

    第三章作业

---
+++ 杨斌/java03/Zy1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Zy1{
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.println("请输入一个三位数整数:");
+		int j=input.nextInt();
+		int a=j%10,b=j/10%10,c=j/100%10;
+		if(c>b&&b>a){
+			System.out.print("是降序数");
+
+		}else{
+			System.out.print("不是降序数");
+
+
+
+		}
+		
+
+
+
+
+
+
+
+		}
+
+
+
+}
