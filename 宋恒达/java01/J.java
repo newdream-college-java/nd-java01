418170d5fe8a883f3238316dc1ee9fdb533d5eeb
chan-kate <ccsu_loader@163.com>
2019-02-24 12:05:15 +0800

    删除主分支

--- 宋恒达/java01/J.java
+++
@@ -1,9 +0,0 @@
-public class J{
-	public static void main(String[] args){
-		System.out.println("\t欢迎使用我行我素购物系统");
-		System.out.println("\t    1、客户信息管理");
-		System.out.println("\t    2、购物结算");
-		System.out.println("\t    3、真情回馈");
-		System.out.println("\t    4、注销");
-	}
-}
--- 杨斌/java01/A.java
+++
@@ -1,5 +0,0 @@
-public class A{
-	public static void main(String[] args){
-		System.out.print("天王盖地虎，宝塔镇河妖");
-	}
-}
