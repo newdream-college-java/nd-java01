2758b5b26aefa7a38d93a434c1b0306f61dbd982
lijiancong <465557831@qq.com>
2019-02-25 13:08:23 +0800

    第二章作业提交

---
+++ 李健聪/java02/B.java
@@ -0,0 +1,13 @@
+public class B{
+	public static void main(String[] args){
+		String a;
+		String b;
+		String c;
+		String d;
+		a="这条狗叫旺财";
+		b="颜色是白色的";
+		c="已经一岁了";
+		d="是个公狗";
+		System.out.println(a+b+c+d);
+	}
+}
