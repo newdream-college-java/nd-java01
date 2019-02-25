a112f54143507efcf9312bcc99491f72d3bb1aa9
LiuYang87483587 <87483587@qq.com>
2019-02-25 10:52:25 +0800

    提交第1次作业

---
+++ 刘阳/java01/C.java
@@ -0,0 +1,11 @@
+public class C{
+	public static void main(String[] args){
+	System.out.println("   *");
+	System.out.println("  ***");
+	System.out.println(" *****");
+	System.out.print("*******");
+	}
+
+
+
+}
