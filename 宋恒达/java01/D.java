b0553861ecfff360f8812c76cb456d1c1bda8ff7
songhengda <24423848761@qq.com>
2019-02-25 13:45:25 +0800

    第一章作业

---
+++ 宋恒达/java01/D.java
@@ -0,0 +1,7 @@
+public class D{
+	public static void main(String[] args){
+		System.out.print("我的名字:\t小样\n");
+		System.out.print("我的性别:\t男\n");
+		System.out.print("我的年龄:\t18");
+	}
+}
---
+++ 宋恒达/java01/H.java
@@ -0,0 +1,8 @@
+public class H{
+	public static void main(String[] args){
+		System.out.println("\t\t\t\t购物清单");
+		System.out.println("商品名称\t\t购买数量\t\t商品单价\t\t金额");
+		System.out.println("鳄鱼夹克\t\t 1 \t\t\t1900\t\t\t1900");
+		System.out.println("ipodMp4\t\t\t 2 \t\t\t400\t\t\t800");
+	} 
+}
