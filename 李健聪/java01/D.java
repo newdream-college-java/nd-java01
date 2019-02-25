be5e75fdd42e12868d26725366e4f85a198d8b0f
lijiancong <465557831@qq.com>
2019-02-25 13:07:02 +0800

    第一章作业提交

---
+++ 李健聪/java01/D.java
@@ -0,0 +1,7 @@
+public class D{
+	public static void main(String[] args){
+		System.out.print("我的姓名：\t小样\n");
+		System.out.print("我的性别：\t男\n");
+		System.out.print("我的年龄：\t18\n");	
+	}
+}
---
+++ 李健聪/java01/课后作业/C.java
@@ -0,0 +1,12 @@
+public class C{
+	public static void main(String[] args){
+		System.out.println("\n\n");
+		System.out.println("\t\t欢迎使用我行我素购物管理系统\n\n\n");	
+		System.out.println("*****************************************************************");
+		System.out.println("\t\t\t1.客户信息管理\n\n");
+		System.out.println("\t\t\t2.购物结算\n\n");
+		System.out.println("\t\t\t3.真情回馈\n\n");
+		System.out.println("\t\t\t4.注销\n\n");
+		System.out.println("*****************************************************************");
+	}
+}
