d91a5fb1b245852fa258cd4e3a09a0f6bde3447f
waimianxiayule <1452941741@qq.com>
2019-02-26 17:25:08 +0800

    第七章作业

---
+++ 杨磊/java7/work6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Work6{
+	public static void main(String []args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("请输入一个整数n：");
+		int a= input.nextInt();
+		int count=0,m=0;
+		for(int i=1;i<=a;i++){
+			if(i%2==0){
+				count++;
+			}
+			if(!(i%2==0)){
+				m++;
+			}	
+
+		}
+		System.out.print("1到"+a+"的偶数有"+count+"个\t"+"1到"+a+"的奇数有"+m+"个\t");
+	}
+}
