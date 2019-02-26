7c7b4e0972b349b39950586c3018c3ab55b7562b
lyg2019 <ccsu_916390120@qq.com>
2019-02-26 19:54:47 +0800

    完成第八次作业

---
+++ 刘勇国/java08/Java03.java
@@ -0,0 +1,10 @@
+public class Java03{
+	public static void main(String[] args){
+		for(int i=1;i<=365;i++){
+			for(int j=1;j<=3;j++){
+				System.out.println("吃了一顿饭");
+			}
+			System.out.println("总算又过了一天");
+		}
+	}
+}
