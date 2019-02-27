daf8f522a8a8b6a36e3196ea1d2d0e59d9c5f939
waimianxiayule <1452941741@qq.com>
2019-02-28 00:27:17 +0800

    第八章作业

---
+++ 杨磊/java8/Work6.java
@@ -0,0 +1,17 @@
+/*¥Ú”°Õº–Œ
+**************
+**************
+**************
+**************
+*/
+public class Work6{
+	public static void main(String []args){
+		for(int i=1;i<=4;i++){
+			for(int j=1;j<=14;j++){
+				System.out.print("*");
+
+			}
+		System.out.println();
+		}
+	}
+}
