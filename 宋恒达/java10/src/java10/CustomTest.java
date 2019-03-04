b462ee2c9d5181ec90a354410ff67517e944a7fa
songhengda <24423848761@qq.com>
2019-03-04 17:07:57 +0800

    第10章作业

---
+++ 宋恒达/java10/src/java10/CustomTest.java
@@ -0,0 +1,14 @@
+package java10;
+/**
+ * ²âÊÔCustomÀà
+ * @author mo
+ *
+ */
+public class CustomTest {
+	public static void main(String[] args) {
+		Custom custom=new Custom();
+		custom.style="½ğ¿¨";
+		custom.integral=3050;
+		custom.show();
+	}
+}
