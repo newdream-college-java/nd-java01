c2a6bf3f0e61f9b04ff39f95117c302078fdd345
Dyekong <18173483397@163.com>
2019-03-05 19:45:56 +0800

    第十二章作业

---
+++ 段瑜/java12/Test6.java
@@ -0,0 +1,16 @@
+package ÈË»ú²ÂÈ­;
+
+import java.util.Scanner;
+
+public class Test6 {
+	private static int i;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		Test5 a1=new Test5();
+		a1.show();
+		
+	}
+
+}
