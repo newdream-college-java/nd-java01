86080248ff8655ab000105cb6a3731394afaa8b3
ChenHui666 <1355631356@qq.com>
2019-03-06 09:35:15 +0800

    第十一节

---
+++ 常德陈辉/java.11/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+	double num1;
+	double num2;
+	double sum=0;
+	public double showAdd(){
+		sum=num1+num2;
+		return sum;
+	}
+	public double showMinus(){
+		sum=num1-num2;
+		return sum;
+	}
+	public double showMultiple(){
+		sum=num1*num2;
+		return sum;
+	}
+	public double showDivide(){
+		sum=num1/num2;
+		return sum;
+	}
+}
