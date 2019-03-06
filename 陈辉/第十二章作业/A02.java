43351bc9c0010ccb51c54fe43521fb021ce92b7e
chenhui120 <1205549467@qq.com>
2019-03-06 22:05:38 +0800

    提交十二章作业  算是看着来

---
+++ 陈辉/第十二章作业/A02.java
@@ -0,0 +1,31 @@
+public class Person {
+    // 属性----名字，积分
+    String name="帝皇侠";
+    int score;
+
+    // 方法--出拳
+    public int showFist() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请出拳：1：石头，2：剪刀，3：布：");
+        int choice = -1;
+        //输入是非数字？
+        if(input.hasNextInt()) {
+            choice  = input.nextInt();
+        }
+        switch (choice) {
+            case 1:
+                System.out.println("你出拳：石头");
+                break;
+            case 2:
+                System.out.println("你出拳：剪刀");
+                break;
+            case 3:
+                System.out.println("你出拳：布");
+                break;
+             default:
+                 System.out.println("亲，你输入有误！！");
+                break;
+        }
+       return choice;
+    }
+}
---
+++ 陈辉/第十二章作业/A03.java
@@ -0,0 +1,8 @@
+public class StartGuess {
+    public static void main(String[] args) { 
+        Game g = new Game();//新建一个游戏
+        g.initial();//游戏初始化
+        g.startGame();//开始游戏
+        
+    }
+}
