2d9d7de20f39429fd45d3c1019dfd004c760701a
lyg2019 <ccsu_916390120@qq.com>
2019-03-08 08:51:02 +0800

    第十四章作业完成

---
+++ 刘勇国/java14/Student2Test.java
@@ -0,0 +1,10 @@
+package 第14章封装作业;
+
+public class Student2Test {
+	public static void main(String[] args) {
+		Student2 st = new Student2("ss", 22);
+		st.showMyself();
+		Student2 st2 = new Student2("ss", 22,"女","会计");
+		st2.showMyself();
+	}
+}
