158448d801c69ac18b1bd9fb79eb38fe9b5ee15c
1998wjh <1004115214@qq.com>
2019-03-08 08:28:05 +0800

    第十一章

---
+++ 作业/java11/第一题/Test01.java
@@ -0,0 +1,12 @@
+package µÚ11ÕÂ×÷Òµ;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Manager m1 = new Manager();
+		m1.name="newdream";
+		m1.passWord="0000";
+		System.out.println( m1.show());
+		String a = m1.show();
+		System.out.println(a);
+	}
+}
---
+++ 作业/java11/第二题/Test02.java
@@ -0,0 +1,10 @@
+package µÚ11ÕÂ×÷Òµ;
+
+public class Test02 {
+	public static void main(String[] args) {
+		Homework02_2 h1 = new Homework02_2();
+		h1.showLoginMenu();
+		
+
+	}
+}
