6a0ea6102df1a510bd7d3f11f37dcc32e6f4b96a
songhengda <24423848761@qq.com>
2019-03-11 00:18:55 +0800

    第十四章作业

---
+++ 宋恒达/java14/src/java14/Student1Test.java
@@ -0,0 +1,10 @@
+package java14;
+
+public class Student1Test {
+	public static void main(String[] args) {
+		Student1 student1=new Student1();
+		student1.setName("ะกร๗");
+		student1.setAge(18);
+		student1.print();
+	}
+}
