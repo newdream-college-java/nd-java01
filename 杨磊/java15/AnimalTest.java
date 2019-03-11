32a16c4196f1e367960d6673868c4c7831cbbf04
waimianxiayule <1452941741@qq.com>
2019-03-11 22:39:48 +0800

    第十五章作业

---
+++ 杨磊/java15/AnimalTest.java
@@ -0,0 +1,12 @@
+package com.newderam.xin.yl;
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		Fish a = new Fish();
+		a.info(2, 5);
+		Bird b = new Bird();
+		b.info(4, "ºìÉ«");
+	}
+
+}
