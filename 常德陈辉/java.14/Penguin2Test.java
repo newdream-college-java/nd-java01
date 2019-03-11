296a4a0b6f5269bf7b4ccc27e26d3095ace056d0
ChenHui666 <1355631356@qq.com>
2019-03-11 10:04:42 +0800

    第一十四节

---
+++ 常德陈辉/java.14/Penguin2Test.java
@@ -0,0 +1,12 @@
+package zuoye;
+
+
+public class Penguin2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Penguin2 qe=new Penguin2();
+		qe.print();
+	}
+
+}
