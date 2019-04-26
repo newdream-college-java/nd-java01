d8c29acd09541ed3616960b1a11b686582776752
songhengda <24423848761@qq.com>
2019-04-26 15:01:13 +0800

    js第六章上机部分作业

---
+++ 宋恒达/js/js第六章/上机作业素材/练习4素材：淘宝登录页面/login.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+function checkForm( )
+{
+	var strName = document.getElementById("txtName").value;
+	var strPass = document.getElementById("txtPass").value;
+    if(strName.length != 0)
+	{	  	
+	  for(var i=0;i<strName.length;i++)
+	  {
+	     var charTest=strName.toLowerCase( ).charAt(i);
+		 if( (!(charTest>='0' && charTest<='9')) &&  (!(charTest>='a' && charTest<='z'))  && (charTest!='_') )
+		 {
+		  alert("会员名包含非法字符，只能包括 a-z、0-9 和下划线");
+		  return false;
+		  }
+	  }
+	}
+	else
+	{
+		alert("请输入用户名");
+		return false;
+	}
+	
+	if(strPass== ""){
+		alert("未输入密码 \n" + "请输入密码");
+	    return false;
+	    }
+	else{
+		if(strPass.length <6)
+		{
+		alert("密码位数不能少于6位，请重新输入！");
+		return false
+		}
+	 }
+	return true;
+}
