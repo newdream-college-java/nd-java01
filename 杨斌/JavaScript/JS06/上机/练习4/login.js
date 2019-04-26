db48d45ef9ac5e63c319df296fad1459289b06fa
yangbin1997 <82408624@qq.com>
2019-04-26 20:15:37 +0800

    完成js全部作业

---
+++ 杨斌/JavaScript/JS06/上机/练习4/login.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function checkForm(){
+	var strName = document.getElementById("txtName").value;
+	var strPass = document.getElementById("txtPass").value;
+    // if(stuName=""){
+    // 	alert("322");
+    // 	return false;
+    // }
+    return false;
+}
