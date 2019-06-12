8e828bcb4b3750bcc555ad57386e96a63d63aef2
waimianxiayule <1452941741@qq.com>
2019-06-12 11:42:20 +0800

    19/6/11实体类修改后提交

---
+++ src/cn/song/entity/News.java
@@ -0,0 +1,10 @@
+package cn.song.entity;
+
+/*新闻表*/
+public class News {
+	private int nId;// 新闻id
+	private String nTitle;// 新闻标题
+	private String nContent;// 新闻内容
+	private String nTime;// 发表时间
+	private int nType;// 新闻类型，0表示普通新闻，1代表网站公告
+}
---
+++ src/cn/song/entity/Station.java
@@ -0,0 +1,11 @@
+package cn.song.entity;
+
+public class Station {
+	private int sId;// 车站信息表主键
+	private String sName;// 车站名
+	private String sIntroduction;// 车站介绍
+	private String sAddress;// 车站地址
+	private String sPhone;// 车站联系电话
+	private String sBus;// 途经公交
+	private String sOpenTime;// 开通时间
+}
