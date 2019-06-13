1d5210841e3b42e6d03a5dbebf5f6db32b1030f0
waimianxiayule <1452941741@qq.com>
2019-06-13 20:22:48 +0800

    1

--- src/cn/song/controler/ChepiaoServlet.java
+++ src/cn/song/controler/ChepiaoServlet.java
@@ -9,9 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.song.dao.ChepiaoProblemDao;
 import cn.song.dao.StationDao;
+import cn.song.dao.impl.ChepiaoProblemDaoImpl;
 import cn.song.dao.impl.StationDaoImpl;
 import cn.song.entity.Station;
+import cn.song.vo.ChepiaoProblem;
 
 public class ChepiaoServlet extends HttpServlet {
 
@@ -24,11 +27,12 @@ public class ChepiaoServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 新开通车站部分代码开始
 		StationDao sta = new StationDaoImpl();
 		List<Station> list = new ArrayList<Station>();
 		List<Station> list1 = new ArrayList<Station>();
 		List<Station> list2 = new ArrayList<Station>();
-		list = sta.GetStation();
+		list = sta.getStation();
 		int i = 1;
 		for (Station station : list) {
 			if (i <= 2) {
@@ -43,6 +47,12 @@ public class ChepiaoServlet extends HttpServlet {
 		}
 		req.setAttribute("list1", list1);
 		req.setAttribute("list2", list2);
+		// 新开通车站部分代码结束
+		// 问题列表开始
+		ChepiaoProblemDao chepiaoProblem = new ChepiaoProblemDaoImpl();
+		List<ChepiaoProblem> chepiaoProblemList = new ArrayList<ChepiaoProblem>();
+		chepiaoProblemList = chepiaoProblem.getChepiaoProblem();
+		req.setAttribute("chepiaoProblemList", chepiaoProblemList);
 		req.getRequestDispatcher("che-piao.jsp").forward(req, resp);
 	}
 }
--- src/cn/song/dao/StationDao.java
+++ src/cn/song/dao/StationDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import cn.song.entity.Station;
 
 public interface StationDao {
-	public List<Station> GetStation();
+	public List<Station> getStation();
+
 	Station station(int id);
 }
