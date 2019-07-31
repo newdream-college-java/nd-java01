bae1fc7ae4b8c2eded5024940b3265a4e9ae0830
chenbiao <994700146@qq.com>
2019-07-31 23:09:26 +0800

    作业

---
+++ 陈飙/框架作业/mywebn/src/com/yb/entity/Fwxx.java
@@ -0,0 +1,145 @@
+package com.yb.entity;
+
+import java.util.Date;
+
+public class Fwxx {
+    private Integer fwid;
+
+    private Integer uid;
+
+    private Integer jdid;
+
+    private Integer qxid;
+
+    private Integer lxid;
+
+    private Integer shi;
+
+    private Integer ting;
+
+    private String fwxx;
+
+    private Long zj;
+
+    private String title;
+
+    private Date date;
+
+    private String telephone;
+
+    private String concat;
+
+    public Integer getFwid() {
+        return fwid;
+    }
+
+    public void setFwid(Integer fwid) {
+        this.fwid = fwid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getJdid() {
+        return jdid;
+    }
+
+    public void setJdid(Integer jdid) {
+        this.jdid = jdid;
+    }
+
+    public Integer getQxid() {
+        return qxid;
+    }
+
+    public void setQxid(Integer qxid) {
+        this.qxid = qxid;
+    }
+
+    public Integer getLxid() {
+        return lxid;
+    }
+
+    public void setLxid(Integer lxid) {
+        this.lxid = lxid;
+    }
+
+    public Integer getShi() {
+        return shi;
+    }
+
+    public void setShi(Integer shi) {
+        this.shi = shi;
+    }
+
+    public Integer getTing() {
+        return ting;
+    }
+
+    public void setTing(Integer ting) {
+        this.ting = ting;
+    }
+
+    public String getFwxx() {
+        return fwxx;
+    }
+
+    public void setFwxx(String fwxx) {
+        this.fwxx = fwxx == null ? null : fwxx.trim();
+    }
+
+    public Long getZj() {
+        return zj;
+    }
+
+    public void setZj(Long zj) {
+        this.zj = zj;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+
+    public String getConcat() {
+        return concat;
+    }
+
+    public void setConcat(String concat) {
+        this.concat = concat == null ? null : concat.trim();
+    }
+
+	@Override
+	public String toString() {
+		return "Fwxx [fwid=" + fwid + ", uid=" + uid + ", jdid=" + jdid
+				+ ", qxid=" + qxid + ", lxid=" + lxid + ", shi=" + shi
+				+ ", ting=" + ting + ", fwxx=" + fwxx + ", zj=" + zj
+				+ ", title=" + title + ", date=" + date + ", telephone="
+				+ telephone + ", concat=" + concat + "]";
+	}
+    
+}
---
+++ 陈飙/框架作业/mywebn/src/com/yb/servlet/FwxxListServlet.java
@@ -0,0 +1,62 @@
+package com.yb.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.github.pagehelper.PageHelper;
+import com.yb.entity.Fwxx;
+import com.yb.entity.FwxxExample;
+import com.yb.mapper.FwxxMapper;
+
+public class FwxxListServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 311689750909577323L;
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//编码
+		response.setContentType("text/html");
+		request.setCharacterEncoding("utf-8");
+		//得到page页数
+		String pageStr = request.getParameter("page");
+		//加载配置文件得到sqlSessionFactory工厂
+		InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		//得到sqlSession调用查询方法
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		FwxxMapper mapper = sqlSession.getMapper(FwxxMapper.class);
+		FwxxExample example=new FwxxExample();
+		Integer page=1;
+		if(pageStr!=null&&pageStr.trim().length()>0){
+			page=Integer.parseInt(pageStr);
+		}
+		//加载分页插件
+		PageHelper.startPage(page, 3);
+		//得到返回集合
+		List<Fwxx> selectByExample = mapper.selectByExample(example);
+		
+		//得到总页数
+		int countBySelect = mapper.countBySelect();
+		request.setAttribute("selectByExample", selectByExample);
+		request.setAttribute("page", page);
+		request.setAttribute("countBySelect", countBySelect);
+		request.getRequestDispatcher("my.jsp").forward(request, response);
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
