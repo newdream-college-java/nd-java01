2c597f7f203920ee3baa8db6ae6914ccced47bc8
yangbin1997 <82408624@qq.com>
2019-07-31 21:19:01 +0800

    页面增删改查

---
+++ 杨斌/Mybatis/mybatis05/mywebn/src/com/yb/entity/User.java
@@ -0,0 +1,33 @@
+package com.yb.entity;
+
+public class User {
+    private Integer uid;
+
+    private String uname;
+
+    private String upass;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname == null ? null : uname.trim();
+    }
+
+    public String getUpass() {
+        return upass;
+    }
+
+    public void setUpass(String upass) {
+        this.upass = upass == null ? null : upass.trim();
+    }
+}
